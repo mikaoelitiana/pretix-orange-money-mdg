87f712e021ab3b627529e49058ae35c8365d8117
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2024-11-06 23:13:34 +0100

    fix: verbose name

--- pretix_orange_money_mdg/payment.py
+++ pretix_orange_money_mdg/payment.py
@@ -1,13 +1,19 @@
 from collections import OrderedDict
 
+from django import forms
+from django.utils.translation import gettext as __, gettext_lazy as _
+
+from pretix.base.models import Event
 from pretix.base.payment import BasePaymentProvider
+from pretix.base.settings import SettingsSandbox
 
 class OrangeMoneyMadagascar(BasePaymentProvider) :
     identifier = 'orange_money_madagascar'
-    verbose_name = _('OrangeMoneyMadagascar')
+    verbose_name = _('Orange Money Madagascar')
     payment_form_fields = OrderedDict([
     ])
 
     def __init__(self, event: Event):
         super().__init__(event)
         self.settings = SettingsSandbox('payment', 'orange_money_madagascar', event)
+
